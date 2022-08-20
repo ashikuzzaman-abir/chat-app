09f7d3df881c56a5249d607fb605e4a900f44fc2
Ashikuzzaman Abir <ashikuzzamanabir@outlook.com>
2022-08-21 05:53:03 +0600

    socket error resolved

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 
 import {Routes, Route} from 'react-router-dom';
-import socket from './configs/socket.config'
+// import socket from './configs/socket.config'
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
